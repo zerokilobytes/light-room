c171e3eb141977a6be9c401ff7f0aa2296fc85c6
zerokilobytes <markel.mairs@gmail.com>
2014-01-05 16:17:29 -0500

    added blur

--- js/Demo.js
+++ js/Demo.js
@@ -49,24 +49,17 @@ var oldPt;
 var oldMidPt;
 var displayCanvas;
 var image;
-var bitmap;
 var maskFilter;
 var cursor;
 var text;
-var blur;
+var light;
 
 function init2() {
-    
-
-
     image = new Image();
     image.onload = handleComplete2;
-    image.src = "images/wall.jpg";
+    image.src = "images/white.png";
 
     stage = gameContext.stage;
-    text = new createjs.Text("Loading...", "20px Arial", "#999999");
-    text.set({x: gameContext.stage.canvas.width / 2, y: gameContext.stage.canvas.height - 80});
-    text.textAlign = "center";
 }
 
 function handleComplete2() {
@@ -76,56 +69,36 @@ function handleComplete2() {
 
     stage.addEventListener("stagemousedown", handleMouseDown);
     stage.addEventListener("stagemouseup", handleMouseUp);
-    stage.addEventListener("stagemousemove", handleMouseMove);
     drawingCanvas = new createjs.Shape();
-    bitmap = new createjs.Bitmap(image);
+    
+    light = new createjs.Bitmap(image);
+    light.filters = [new createjs.BlurFilter(15, 15, 2)];
+    light.cache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
+    light.alpha = 0.5;
 
-    blur = new createjs.Bitmap(image);
-    blur.filters = [new createjs.BlurFilter(15, 15, 2)];
-    blur.cache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
-    blur.alpha = 0.5;
 
-    text.text = "Welcome to the dark room";
-
-    stage.addChild(blur, text, bitmap);
+    stage.addChild(light);
     updateCacheImage(false);
 
     cursor = new createjs.Shape(new createjs.Graphics().beginFill("#FFFFFF").drawCircle(0, 0, 50));
     cursor.cursor = "pointer";
-
-    //stage.addChild(cursor);
 }
 
 function handleMouseDown(event) {
     oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
     oldMidPt = oldPt;
     isDrawing = true;
+
+    handleMouseMove(event);
 }
 
 function handleMouseMove(event) {
-    cursor.x = stage.mouseX;
-    cursor.y = stage.mouseY;
-
     if (!isDrawing) {
         stage.update();
         return;
     }
-
-    var midPoint = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
-
-    drawingCanvas.graphics.setStrokeStyle(100, "round", "round")
-            .beginStroke("rgba(0,0,0,0.15)")
-            .moveTo(midPoint.x, midPoint.y)
-            .curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
-
-    oldPt.x = stage.mouseX;
-    oldPt.y = stage.mouseY;
-
-    oldMidPt.x = midPoint.x;
-    oldMidPt.y = midPoint.y;
-
+    drawingCanvas.graphics.beginFill("rgba(0,0,0,0.12)").drawCircle(stage.mouseX, stage.mouseY, 100);
     updateCacheImage(true);
-
 }
 
 function handleMouseUp(event) {
@@ -142,13 +115,13 @@ function updateCacheImage(update) {
 
     maskFilter = new createjs.AlphaMaskFilter(drawingCanvas.cacheCanvas);
 
-    bitmap.filters = [maskFilter];
-    if (update) {
-        bitmap.updateCache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
-    } else {
-        bitmap.cache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
-    }
+    light.filters = [maskFilter];
 
+    if (update) {
+        light.updateCache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
+    } else {
+        light.cache(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
+    }
+    light.sourceRect = new createjs.Rectangle(0, 0, gameContext.settings.screeSize.width, gameContext.settings.screeSize.height);
     stage.update();
 }
-
