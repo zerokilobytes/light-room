716beb40f10e854fde6cb5644ec9f46fcc1ee9f3
zerobytes <markel.mairs@gmail.com>
2014-01-07 18:11:56 -0500

    add motion

--- js/Fixture/AntiqueFrame.js
+++ js/Fixture/AntiqueFrame.js
@@ -4,6 +4,8 @@ var AntiqueFrame = function(context) {
     this.body = null;
     this.active = false;
     this.bulb = null;
+    this.interval = 0;
+    this.direction = 1;
     this.init();
 };
 
@@ -18,6 +20,7 @@ AntiqueFrame.prototype = {
         this.spawn(positionVector);
     },
     spawn: function(positionVector) {
+        var _this = this;
         this.enabled = true;
         var frame = Resource.get("frame");
         var background = Resource.get("mona_bg");
@@ -33,18 +36,46 @@ AntiqueFrame.prototype = {
         bgBitmap.x = positionVector.x;
         bgBitmap.y = positionVector.y;
 
-        var mona1Bitmap = this.createSkin(mona1);
-        mona1Bitmap.x = positionVector.x;
-        mona1Bitmap.y = positionVector.y;
+        this.mona1Bitmap = this.createSkin(mona1);
+        this.mona1Bitmap.x = positionVector.x;
+        this.mona1Bitmap.y = positionVector.y;
+
+        //this.mona1Bitmap.skewX = -10;
+        //mona1Bitmap.skewY = 2;
 
         this.context.stage.addChild(bgBitmap);
-        this.context.stage.addChild(mona1Bitmap);
+        this.context.stage.addChild(this.mona1Bitmap);
         this.context.stage.addChild(frameBitmap);
 
         Entity.prototype.spawn.call(this);
+
+        var timer = function move() {
+//            if (_this.lapse > 0 && _this.lapse % 2 === 0) {
+//                // console.log(_this.lapse);
+//            }
+//            else {
+//
+//            }
+
+            if (_this.interval >= 3) {
+                _this.direction = _this.direction * -1;
+            }
+
+            if (_this.interval <= -3) {
+                _this.direction = 1;
+            }
+
+            setTimeout(timer, 500);
+            _this.interval = _this.interval + _this.direction;
+        };
+
+        timer();
     },
     update: function() {
-        //Entity.prototype.update.call(this);
+        var sin = Math.sin(MathFunc.toRadiant(this.interval));
+        var cos = Math.cos(MathFunc.toRadiant(this.interval));
+        this.mona1Bitmap.skewY = sin * 100;
+        //this.mona1Bitmap.skewX = cos * 1 * this.direction;
     },
     getSkin: function() {
         return Entity.prototype.getSkin.call(this);
